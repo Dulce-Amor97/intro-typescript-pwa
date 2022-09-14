604c076e826c6a558fb8fac013fcd357988b3fef
dulce-amor97 <dulceamorgomezmunoz@gmail.com>
2022-09-13 21:01:22 -0500

    cuarto commit

---
+++ intro-typescript-pwa/src/index.ts
@@ -0,0 +1,26 @@
+//Objeto con ts
+
+//Definicion de interface
+interface Alumno{
+    matricula:number;
+    nombre: string;
+    email: string;
+}
+
+let alumno: Alumno = {
+    nombre: 'Dulce',
+    matricula: 19002508,
+    email: "dulceamorgomezmunoz@gmail.com"
+}
+
+//Arreglos
+let mascotas=['perro', 'gato', 'panda']
+
+mascotas[1] = 'shshsh';
+mascotas.push('nuevo')
+
+let tem:(number | string)[]=[]
+tem.push('Nombre');
+tem.push(246);
+
+console.log(alumno)
