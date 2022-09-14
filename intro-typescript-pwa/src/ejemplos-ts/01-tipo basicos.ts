4e63e11ec85111b8e3bfdb7e51fe22de7f9ecd17
dulce-amor97 <dulceamorgomezmunoz@gmail.com>
2022-09-13 20:45:11 -0500

    tercero commit

---
+++ intro-typescript-pwa/src/ejemplos-ts/01-tipo basicos.ts
@@ -0,0 +1,22 @@
+//variables tipos de datos
+
+var num1 = 2;
+let num2 = 3;
+const num3 = 4;
+
+/*function suma(){
+    const tem = 3;
+    console.log(tem);
+    tem=97;
+}
+
+
+suma();*/
+
+let nombre:string ='Dulce';
+let num:number=21;
+let activo:boolean=true;
+
+let matricula:number | string | boolean = "971jkv";
+matricula = 19002508;
+matricula= false;
